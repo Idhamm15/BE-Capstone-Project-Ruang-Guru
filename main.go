9b761564b6bddf90487b5cfa8ba867bf2a387b73
Idhamm15 <idhamtamvanz123@gmail.com>
2024-06-16 02:36:56 +0700

    change ports

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-    "log"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/gin-gonic/gin"
-    "github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
-    "be_capstone/handlers"
+	"be_capstone/handlers"
 )
+
+func envPortOr(port string) string {
+	// If `PORT` variable in environment exists, return it
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	// Otherwise, return the value of `port` variable from function argument
+	return ":" + port
+}
 
 func main() {
     // Set mode release jika tidak dalam debug
@@ -18,8 +29,6 @@ func main() {
     }
 
     r := gin.Default()
-    r.ForwardedByClientIP = true
-    r.SetTrustedProxies([]string{"127.0.0.1"})
 
     chatHandler := handlers.NewChatHandler()
     r.GET("/", func(c *gin.Context) {
@@ -30,5 +39,14 @@ func main() {
 	})
     r.POST("/chat", chatHandler.HandleChat)
 
-    r.Run(":5000")
+    // Set trusted proxies
+	if err := r.SetTrustedProxies(nil); err != nil {
+		panic(err)
+	}
+
+	// Use `PORT` provided in environment or default to 3000
+	var port = envPortOr("3000")
+
+	// Mulai server
+	log.Fatal(http.ListenAndServe(port, r))
 }
